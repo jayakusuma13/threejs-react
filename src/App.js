bc61ebf7ba31e1c2316c6567d3c35a49d9adab39
unknown <kusuma844@gmail.com>
2023-01-27 10:36:10 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
+import { Sky } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei';
+import { useState, useRef } from 'react';
+import styles from './index.css'
 
-function App() {
+function Box(props){
+  
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  //useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+
+  return(
+    <mesh {...props}
+    ref={ref}
+    scale={clicked ? 1.5 : 1}
+    onClick={(event) => click(!clicked)}
+    onPointerOver={(event) => hover(true)}
+    onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]}/>
+      <meshLambertMaterial attach="material" color="hotpink"/>
+    </mesh>
+  )
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text'></input>
+      <Canvas>
+        <OrbitControls></OrbitControls>
+        <ambientLight intensity={0.5}></ambientLight>
+        <Sky sunPosition={[100,100,20]}></Sky>
+        <Box position={[-1.2, 0, 0]}></Box>
+        <Box position={[1.2, 0, 0]}></Box>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
